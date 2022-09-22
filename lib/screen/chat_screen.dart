c279d4b97635b18bd6ca47aa8a9c9bfff7a64c12
EhtshamNizamani <ehtshamnizamani89@gmail.com>
2022-09-22 12:07:48 +0800

    Title Change

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -62,7 +62,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FlutterChat'),
+        title: const Text('Flutter Chat App'),
         actions: [
           DropdownButton(
             underline: Container(),
